6625ed753b0a6f9a882cefafd7fa4b2104d0b6af
IvanBrekman <nikitka-zhukov03@mail.ru>
2021-10-04 18:58:26 +0300

    Fixed test functions

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "tests/test.h"
 
 int main(int argc, char** argv) {
-    int exit_code = error_with_second_dtor_call_test();
+    int exit_code = normal_work_test();
     printf("exit code: %d", exit_code);
 
     return 1;
